06622ad9c86e684539d98a17c398bb2420b92bf2
pattyish <patrickishimwe10@gmail.com>
2020-01-12 15:13:14 +0200

    changes

--- Controller/editEmployeeRecord.js
+++ Controller/editEmployeeRecord.js
@@ -26,6 +26,7 @@ export default class EditEmployeeRecord {
         .json({ status: 400, message: "national id should be 16 digits" });
     }
     const changesOnEmployee = new EmployeeSchema(body);
+    changesOnEmployee.position = body.position;
     const updateEmployee = await DbQuery.editData(id, changesOnEmployee);
     if (!updateEmployee)
       return res
